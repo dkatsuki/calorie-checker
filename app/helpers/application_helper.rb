dcc2888d6745b7e667b29fa751f19519292b7729
dkatsuki <oniku.tabetai813@gmail.com>
2023-01-31 00:23:39 +0900

    stylesheet周り修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def each_page_stylesheet_link_tag(path)
+    content_for :each_page_stylesheet_link_tag, stylesheet_link_tag(path, "data-turbo-track": "reload")
+  end
 end
