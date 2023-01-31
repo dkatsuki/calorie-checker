eebcce1e02176a29d6a3d91916c362501cfc8550
dkatsuki <oniku.tabetai813@gmail.com>
2023-02-01 03:48:49 +0900

    delete実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,20 +8,60 @@ class ApplicationController < ActionController::Base
   end
 
   def index
+    flash.now[:messages] = params[:flash]&.[](:messages)
     @records = self.model.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @records }
+    end
   end
 
   def new
     @record = self.model.new
   end
 
+  def show
+    flash.now[:messages] = params[:flash]&.[](:messages)
+    @record = self.model.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @record }
+    end
+  end
+
   def create
     @record = self.model.new(self.strong_parameters)
 
-    if @record.save
-      redirect_to action: :index
+    respond_to do |format|
+      if @record.save
+        format.html { redirect_to action: :index, flash: {messages: ["#{@record.class.name}のレコード作成に成功しました。"] }}
+        format.json { render :show, status: :created, location: @record }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @record.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @record = self.model.find_by(id: params[:id])
+
+    if @record.destroy
+      flash[:message] = "削除に成功しました。"
     else
-      render action: :new
+      flash[:danger] = "削除に失敗しました。"
+    end
+
+    respond_to do |format|
+      if @record.destroy
+        format.html { redirect_to action: :index, flash: {messages: ["#{@record.class.name}の削除に成功しました。"] }, status: :see_other }
+        format.json { head :no_content }
+      else
+        format.html { redirect_to action: :index, flash: {messages: ["#{@record.class.name}の削除に失敗しました。"] }, status: :see_other }
+        format.json { render json: @record.errors, status: :unprocessable_entity }
+      end
     end
   end
 end
