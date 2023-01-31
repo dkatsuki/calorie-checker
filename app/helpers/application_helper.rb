0ddae81dbf0ecb2e5ae3dcddb60ccbfbd53e4141
dkatsuki <oniku.tabetai813@gmail.com>
2023-02-01 05:01:23 +0900

    js

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def each_page_stylesheet_link_tag(path)
     content_for :each_page_stylesheet_link_tag, stylesheet_link_tag(path, "data-turbo-track": "reload")
   end
+
+  def each_page_javascript_import_tag(path)
+    content_for :each_page_javascript_import do
+      javascript_import_module_tag(path)
+    end
+  end
 end
