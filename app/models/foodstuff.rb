a36e8065e8742a6d3ecb8e3a9d1b5be76c6f2006
dkatsuki <oniku.tabetai813@gmail.com>
2023-02-01 04:50:10 +0900

    validateion

--- app/models/foodstuff.rb
+++ app/models/foodstuff.rb
@@ -1,3 +1,10 @@
 class Foodstuff < ApplicationRecord
+  # 栄養素は100gあたり
   validates :name, presence: {message: '食材名の入力は必須です。'}
+  validates :name, uniqueness: {message: '既にこの食材名は登録されています。'}
+  validates :calorie, presence: {message: '100gあたりのカロリーは入力必須です'}
+  validates :carbohydrate, presence: {message: '100gあたりの炭水化物は入力必須です'}
+  validates :fat, presence: {message: '100gあたりの脂質は入力必須です'}
+  validates :protein, presence: {message: '100gあたりのタンパク質は入力必須です'}
+  validates :sugar, presence: {message: '100gあたりの糖質は入力必須です'}
 end
