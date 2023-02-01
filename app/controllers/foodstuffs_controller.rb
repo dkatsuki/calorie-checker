0125481cbd90f563fdb3d89e53857d4dd127ab21
dkatsuki <oniku.tabetai813@gmail.com>
2023-02-01 23:00:53 +0900

    js

--- app/controllers/foodstuffs_controller.rb
+++ app/controllers/foodstuffs_controller.rb
@@ -2,6 +2,12 @@ class FoodstuffsController < ApplicationController
 
   private
     def strong_parameters
-      params.require(:foodstuff).permit(self.model.attribute_names)
+      result = params.require(:foodstuff).permit(self.model.attribute_names)
+      result[:unit_list] = {}
+      result[:unit_list].permit!
+      params[:foodstuff][:unit_list].each do |key, value|
+        result[:unit_list][key] = value
+      end
+      result
     end
 end
--- app/javascript/foodstuffs/new.js
+++ app/javascript/foodstuffs/new.js
@@ -1,53 +1,60 @@
-const nestedForm = document.querySelector('.nested_form')
+const onLoad = () => {
+  const nestedForm = document.querySelector('.nested_form')
 
-const setInputValueToNameKey = (event) => {
-  const delimiter = '[';
-  const splitedBaseName = event.target.name.split(delimiter);
-  splitedBaseName[splitedBaseName.length - 1] = `${event.target.value}]`
-  const newName = splitedBaseName.join(delimiter);
-  event.target.name = newName;
-};
+  const setInputValueToNameKey = (event) => {
+    const delimiter = '[';
+    const splitedBaseName = event.target.name.split(delimiter);
+    splitedBaseName[splitedBaseName.length - 1] = `${event.target.value}]`
+    const newName = splitedBaseName.join(delimiter);
+    event.target.name = newName;
+  };
 
-const getKeyValueCombiElements = () => {
-  return(nestedForm.querySelectorAll('.nested_form > div:not(.add_nested_record_button)'));
-}
-
-const setSetterFunctions = () => {
-  getKeyValueCombiElements().forEach((parent) => {
-    parent.childNodes.forEach((child) => {
-      if (!child.onchange) {
-        child.onchange = setInputValueToNameKey;
-      }
-    })
-  })
-}
-
-setSetterFunctions();
-
-const addNestedRecordButton = nestedForm.querySelector('.add_nested_record_button');
-const createRemoveButton = () => {
-  const removeButton = document.createElement('div');
-  removeButton.classList.add('remove_nested_record_button')
-  removeButton.onclick = (event) => {
-    event.target.parentElement.parentElement.parentElement.remove();
+  const getKeyValueCombiElements = () => {
+    return(nestedForm.querySelectorAll('.nested_form > div:not(.add_nested_record_button)'));
   }
-  const pTag = document.createElement('p');
-  pTag.textContent = '削除';
-  removeButton.appendChild(pTag);
-  return(removeButton);
+
+  const setSetterFunctions = () => {
+    getKeyValueCombiElements().forEach((parent) => {
+      parent.childNodes.forEach((child) => {
+        if (!child.onchange) {
+          child.onchange = setInputValueToNameKey;
+        }
+      })
+    })
+  }
+
+  setSetterFunctions();
+
+  const addNestedRecordButton = nestedForm.querySelector('.add_nested_record_button');
+  addNestedRecordButton.classList.add(Date.now())
+
+  const createRemoveButton = () => {
+    const removeButton = document.createElement('div');
+    removeButton.classList.add('remove_nested_record_button')
+    removeButton.onclick = (event) => {
+      event.target.parentElement.parentElement.parentElement.remove();
+    }
+    const pTag = document.createElement('p');
+    pTag.textContent = '削除';
+    removeButton.appendChild(pTag);
+    return(removeButton);
+  }
+
+  addNestedRecordButton.onclick = () => {
+    const baseElement = getKeyValueCombiElements()[0];
+    const newElement = baseElement.cloneNode(true);
+    const inputElements = newElement.querySelectorAll('input');
+    inputElements.forEach((element) => {
+      element.name = 'foodstuff[unit_list][]';
+      element.id = `${element.id}_${Date.now()}`;
+      element.value = '';
+    })
+
+    newElement.childNodes[1].appendChild(createRemoveButton());
+    nestedForm.insertBefore(newElement, addNestedRecordButton);
+    setSetterFunctions();
+  }
 }
 
-addNestedRecordButton.onclick = () => {
-  const baseElement = getKeyValueCombiElements()[0];
-  const newElement = baseElement.cloneNode(true);
-  const inputElements = newElement.querySelectorAll('input');
-  inputElements.forEach((element) => {
-    element.name = 'foodstuff[unit_list][]';
-    element.id = `${element.id}_${Date.now()}`;
-    element.value = '';
-  })
-
-  newElement.childNodes[1].appendChild(createRemoveButton());
-
-  nestedForm.insertBefore(newElement, addNestedRecordButton);
-}
+window.addEventListener('turbo:load', onLoad)
+window.addEventListener('turbo:submit-end', onLoad)
