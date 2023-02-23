690f758342fd101c1ea149ba80ab89db6bca8cd5
dkatsuki <oniku.tabetai813@gmail.com>
2023-02-24 03:23:03 +0900

    dishフォーム実装まで

--- app/controllers/admin/dishes_controller.rb
+++ app/controllers/admin/dishes_controller.rb
@@ -1,2 +1,15 @@
 class Admin::DishesController < Admin::ApplicationController
+  before_action :set_foodstuffs, only: [:new, :edit]
+
+  def set_foodstuffs
+    @foodstuffs = self.model.get_association_class(:foodstuffs).all
+  end
+
+  private
+    def strong_parameters
+      result = params.require(:foodstuff).permit([
+        *self.model.attribute_names,
+        recipes_attributes: self.model.get_association_class(:recipes).attribute_names
+      ])
+    end
 end
