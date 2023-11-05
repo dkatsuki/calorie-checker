a22c7b9a6630310445370bdc6dfb15d371861651
dkatsuki <oniku.tabetai813@gmail.com>
2023-11-05 15:53:39 +0900

    再計算

--- lib/tasks/modules/calorie_data_fetcher.rb
+++ lib/tasks/modules/calorie_data_fetcher.rb
@@ -1,4 +1,10 @@
 class CalorieDataFetcher
+
+  FAT_CALORIE_PER_GRAM = 9
+  PROTEIN_CALORIE_PER_GRAM = 4
+  CARBOHYDRATE_CALORIE_PER_GRAM = 4
+  ALCOHOL_CALORIE_PER_GRAM = 7
+
   def initialize
     @api_origin = Rails.application.credentials.dig(:api_origins, :calorie_data_source)
     @client = HttpClient.new
@@ -111,30 +117,64 @@ class CalorieDataFetcher
     def scrape_food_data(food_page, id = nil)
       category = food_page.at_css('#centerPositionCcds > div.breadcrumb.subBreadcrumb > div:nth-child(2) > a > span')&.content
       h1 = food_page.at_css('#contentsPadding > div.clearfix h1')
-      food_name = h1.children.first.content
+      food_name = h1.children.first.content.strip
       calorie_data_container = food_page.at_css('#analysis > table > tr:first-child > td')
       elements = calorie_data_container.children.select {|e| e.name != 'br' && e.is_a?(Nokogiri::XML::Element)}.map(&:content)
-      per_100gram_calorie = elements.first
+      per_100gram_calorie = elements.first.gsub(/kcal/, '').to_f
       unit_gram, unit_name = elements.last.gsub(/\)/, '').split('(').map {|e| e.strip}
+      unit_gram = unit_gram.gsub(/g/, '').to_f
       unit_name = unit_gram if unit_name.nil?
-      protein = food_page.at_css('#protein_content').content
-      fat = food_page.at_css('#fat_content').content
-      carbohydrate = food_page.at_css('#carb_content').content
+      protein_per_unit = food_page.at_css('#protein_content').content.to_f
+      fat_per_unit = food_page.at_css('#fat_content').content.to_f
+      carbohydrate_per_unit = food_page.at_css('#carb_content').content.to_f
 
       including_sugar_element = food_page.at_css('#faq > dl > dd:last-child')
-      sugar_per_unit = including_sugar_element.at_css('> strong')&.content.match(/量は(.+)g/)&.[](1)
+      sugar_per_unit = including_sugar_element.at_css('> strong')&.content.match(/量は(.+)g/)&.[](1).to_f
+
+      carbohydrate_calorie_per_unit = carbohydrate_per_unit * CARBOHYDRATE_CALORIE_PER_GRAM
+      fat_calorie_per_unit = fat_per_unit * FAT_CALORIE_PER_GRAM
+      protein_calorie_per_unit = protein_per_unit * PROTEIN_CALORIE_PER_GRAM
+
+      denominator = carbohydrate_calorie_per_unit + fat_calorie_per_unit + protein_calorie_per_unit
+
+      is_alchol = (denominator < ((per_100gram_calorie * (unit_gram / 100)) / 2))
+
+      carbohydrate_per_100gram = nil
+      fat_per_100gram = nil
+      protein_per_100gram = nil
+      sugar_per_100gram = nil
+
+      if is_alchol
+        carbohydrate_per_100gram = carbohydrate_per_unit * (100.to_f / unit_gram)
+        fat_per_100gram = fat_per_unit * (100.to_f / unit_gram)
+        protein_per_100gram = protein_per_unit * (100.to_f / unit_gram)
+        sugar_per_100gram = sugar_per_unit * (100.to_f / unit_gram)
+      else
+        carbohydrate_calorie_ratio = carbohydrate_calorie_per_unit / denominator
+        fat_calorie_ratio = fat_calorie_per_unit / denominator
+        protein_calorie_ratio = protein_calorie_per_unit / denominator
+
+        carbohydrate_per_100gram = (per_100gram_calorie * carbohydrate_calorie_ratio) / CARBOHYDRATE_CALORIE_PER_GRAM
+        fat_per_100gram = (per_100gram_calorie * fat_calorie_ratio) / FAT_CALORIE_PER_GRAM
+        protein_per_100gram = (per_100gram_calorie * protein_calorie_ratio) / PROTEIN_CALORIE_PER_GRAM
+        sugar_per_100gram = carbohydrate_per_100gram * (sugar_per_unit / carbohydrate_per_unit)
+      end
 
       {
         id: id,
         name: food_name,
         category: category,
-        per_100gram_calorie: per_100gram_calorie,
+        per_100gram_calorie: per_100gram_calorie.round(2),
         unit_gram: unit_gram,
         unit_name: unit_name,
-        sugar_per_unit: sugar_per_unit,
-        carbohydrate_per_unit: carbohydrate,
-        fat_per_unit: fat,
-        protein_per_unit: protein,
+        sugar_per_unit: sugar_per_unit.round(2),
+        carbohydrate_per_unit: carbohydrate_per_unit.round(2),
+        fat_per_unit: fat_per_unit.round(2),
+        protein_per_unit: protein_per_unit.round(2),
+        sugar_per_100gram: sugar_per_100gram.round(2),
+        carbohydrate_per_100gram: carbohydrate_per_100gram.round(2),
+        fat_per_100gram: fat_per_100gram.round(2),
+        protein_per_100gram: protein_per_100gram.round(2),
       }
     end
 
